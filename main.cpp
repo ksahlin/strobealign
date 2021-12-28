69e5df4837303ec2356764a317c8d8903fbcf119
Kristoffer Sahlin <krsahlin@gmail.com>
2021-12-28 11:18:45 +0100

    +

--- main.cpp
+++ main.cpp
@@ -2018,7 +2018,7 @@ static inline void get_best_map_location(std::vector<std::tuple<int,nam,nam>> jo
 
 void print_usage() {
     std::cerr << "\n";
-    std::cerr << "StrobeAlign VERSION 0.2\n";
+    std::cerr << "StrobeAlign VERSION 0.2 (bugcheck)\n";
     std::cerr << "\n";
     std::cerr << "StrobeAlign [options] <ref.fa> <reads1.fast[a/q.gz]> [reads2.fast[a/q.gz]]\n";
     std::cerr << "options:\n";
