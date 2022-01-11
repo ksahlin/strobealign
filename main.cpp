3b5c457bbb3abd5abb9f5997aa63c3fcbd1b10a3
Kristoffer Sahlin <krsahlin@gmail.com>
2022-01-11 10:54:28 +0100

    bumped version

--- main.cpp
+++ main.cpp
@@ -2353,7 +2353,7 @@ static inline void get_best_map_location(std::vector<std::tuple<int,nam,nam>> jo
 
 void print_usage() {
     std::cerr << "\n";
-    std::cerr << "StrobeAlign VERSION 0.2.2 (segfault bugfix, with max_diff on seeds) \n";
+    std::cerr << "StrobeAlign VERSION 0.3 (alpha) \n";
     std::cerr << "\n";
     std::cerr << "StrobeAlign [options] <ref.fa> <reads1.fast[a/q.gz]> [reads2.fast[a/q.gz]]\n";
     std::cerr << "options:\n";
