8aa1443049fde5404aecddde3c73546f64497ca8
Marcel Martin <marcel.martin@scilifelab.se>
2022-08-15 14:35:39 +0200

    Add a tiny test dataset (phiX-174) and run a smoke test on it

---
+++ tests/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+# should fail when unknown command-line option used
+#if strobealign -G; then false; fi
+
+# should succeed when only printing help
+strobealign -h
+
+d=tests
+strobealign $d/phix.fasta $d/phix.1.fastq $d/phix.2.fastq | samtools sort -o phix.bam -
+test $(samtools view -c -F 4 phix.bam) = 200
