8191e2a3d018f2fce10027f5e75a292c6b4dcd03
Marcel Martin <marcel.martin@scilifelab.se>
2022-10-28 23:20:27 +0200

    Simplify revcomp_nam_if_needed

--- src/aln.cpp
+++ src/aln.cpp
@@ -619,20 +619,13 @@ static inline bool sort_highest_sw_scores_single(const std::tuple<int, alignment
 bool revcomp_nam_if_needed(nam& n, const Read& read, const References& references, int k) {
     auto read_len = read.size();
     bool fits = false;
-    std::string ref_start_kmer;
-    std::string ref_end_kmer;
-    std::string read_start_kmer;
-    std::string read_end_kmer;
-    std::string read_rc_start_kmer;
-    std::string read_rc_end_kmer;
-    ref_start_kmer = references.sequences[n.ref_id].substr(n.ref_s, k);
-    ref_end_kmer = references.sequences[n.ref_id].substr(n.ref_e-k, k);
+    std::string ref_start_kmer = references.sequences[n.ref_id].substr(n.ref_s, k);
+    std::string ref_end_kmer = references.sequences[n.ref_id].substr(n.ref_e-k, k);
 
     if (!n.is_rc) {
-        read_start_kmer = read.seq.substr(n.query_s, k);
-        read_end_kmer = read.seq.substr(n.query_e-k, k);
-        if ((ref_start_kmer == read_start_kmer) && (ref_end_kmer == read_end_kmer)) {
-//            n.is_rc = false;
+        std::string read_start_kmer = read.seq.substr(n.query_s, k);
+        std::string read_end_kmer = read.seq.substr(n.query_e-k, k);
+        if (ref_start_kmer == read_start_kmer && ref_end_kmer == read_end_kmer) {
             fits = true;
         } else  {
             //  FALSE FORWARD TAKE CARE OF FALSE HITS HERE - it can be false forwards or false rc because of symmetrical hash values
@@ -645,33 +638,30 @@ bool revcomp_nam_if_needed(nam& n, const Read& read, const References& reference
             int q_end_tmp = read_len - n.query_s;
             read_start_kmer = read.rc().substr(q_start_tmp, k);
             read_end_kmer = read.rc().substr(q_end_tmp-k, k);
-            if ((ref_start_kmer == read_start_kmer) && (ref_end_kmer == read_end_kmer)){
+            if (ref_start_kmer == read_start_kmer && ref_end_kmer == read_end_kmer) {
                 fits = true;
                 n.is_rc = true;
                 n.query_s = q_start_tmp;
                 n.query_e = q_end_tmp;
 //                std::cerr << " DETECTED FALSE RC FROM SYMM!! " << std::endl;
             }
-
         }
     } else {
-        read_rc_start_kmer = read.rc().substr(n.query_s, k);
-        read_rc_end_kmer = read.rc().substr(n.query_e-k, k);
-        if ( (ref_start_kmer == read_rc_start_kmer) && (ref_end_kmer == read_rc_end_kmer) ) { // && (ref_segm.substr(n.query_e - k + (ref_diff - read_diff), k) == read_rc.substr(n.query_e - k, k)) ){
+        std::string read_rc_start_kmer = read.rc().substr(n.query_s, k);
+        std::string read_rc_end_kmer = read.rc().substr(n.query_e-k, k);
+        if (ref_start_kmer == read_rc_start_kmer && ref_end_kmer == read_rc_end_kmer) { // && (ref_segm.substr(n.query_e - k + (ref_diff - read_diff), k) == read_rc.substr(n.query_e - k, k)) ){
             n.is_rc = true;
             fits = true;
-        } else{
+        } else {
             //  FALSE REVERSE TAKE CARE OF FALSE HITS HERE - it can be false forwards or false rc because of symmetrical hash values
             //    we need two extra checks for this - hopefully this will remove all the false hits we see (true hash collisions should be very few)
 
             int q_start_tmp = read_len - n.query_e;
             int q_end_tmp = read_len - n.query_s;
-            read_start_kmer = read.seq.substr(q_start_tmp, k);
-            read_end_kmer = read.seq.substr(q_end_tmp-k, k);
-//            std::cerr << " CHECKING2!! " <<   n.query_s << " " <<   n.query_e << " " << std::endl;
-//            std::cerr << read_start_kmer  << " " <<  ref_start_kmer << " " <<  read_end_kmer << " " << ref_end_kmer << std::endl;
+            std::string read_start_kmer = read.seq.substr(q_start_tmp, k);
+            std::string read_end_kmer = read.seq.substr(q_end_tmp-k, k);
 
-            if ((ref_start_kmer == read_start_kmer) && (ref_end_kmer == read_end_kmer)){
+            if (ref_start_kmer == read_start_kmer && ref_end_kmer == read_end_kmer) {
                 fits = true;
                 n.is_rc = false;
                 n.query_s = q_start_tmp;
