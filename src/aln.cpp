b00780f2a77a3ff1b63264f63006abe2ce4a7f2c
Marcel Martin <marcel.martin@scilifelab.se>
2022-10-28 22:36:54 +0200

    Use revcomp_nam_if_needed() within rescue_mate()

--- src/aln.cpp
+++ src/aln.cpp
@@ -1866,75 +1866,7 @@ static inline void rescue_mate(
     auto guide_read_len = guide.size();
     auto read_len = read.size();
 
-    // decide if read should be fw or rc aligned to reference here by checking exact match of first and last strobe in the NAM
-    bool fits = false;
-    std::string ref_start_kmer;
-    std::string ref_end_kmer;
-    std::string read_start_kmer;
-    std::string read_end_kmer;
-    std::string read_rc_start_kmer;
-    std::string read_rc_end_kmer;
-//    std::cerr << "I'm here: n.ref_s:" << n.ref_s << " n.ref_e-k: " << n.ref_e-k <<  " ref_len: " << ref_seqs[n.ref_id].length() << " n.ref_e-k+k: " << n.ref_e-k+k << std::endl;
-
-    ref_start_kmer = references.sequences[n.ref_id].substr(n.ref_s, k);
-    ref_end_kmer = references.sequences[n.ref_id].substr(n.ref_e-k, k);
-
-    if (!n.is_rc) {
-//        std::cerr << "CHECK1: n.query_s:" << n.query_s << " n.query_e-k: " << n.query_e-k <<  " read_len: " << guide_read.length() << " n.query_e-k+k: " << n.query_e-k+k << std::endl;
-
-        read_start_kmer = guide.seq.substr(n.query_s, k);
-        read_end_kmer = guide.seq.substr(n.query_e-k, k);
-        if ((ref_start_kmer == read_start_kmer) && (ref_end_kmer == read_end_kmer)) {
-//            n.is_rc = false;
-            fits = true;
-        } else  {
-            //  FALSE FORWARD TAKE CARE OF FALSE HITS HERE - it can be false forwards or false rc because of symmetrical hash values
-            //    we need two extra checks for this - hopefully this will remove all the false hits we see (true hash collisions should be very few)
-
-//              std::cerr << " CHECKING1!! " << std::endl;
-//            std::cerr << read_start_kmer  << " " <<  ref_start_kmer << " " <<  read_end_kmer << " " << ref_end_kmer << std::endl;
-            // false reverse hit, change coordinates in nam to forward
-
-            int q_start_tmp = guide_read_len - n.query_e;
-            int q_end_tmp = guide_read_len - n.query_s;
-            read_start_kmer = guide.rc().substr(q_start_tmp, k);
-            read_end_kmer = guide.rc().substr(q_end_tmp-k, k);
-            if ((ref_start_kmer == read_start_kmer) && (ref_end_kmer == read_end_kmer)){
-                fits = true;
-                n.is_rc = true;
-                n.query_s = q_start_tmp;
-                n.query_e = q_end_tmp;
-//                std::cerr << " RESCUE DETECTED FALSE RC FROM SYMM!! " << std::endl;
-            }
-
-        }
-    } else {
-        read_rc_start_kmer = guide.rc().substr(n.query_s, k);
-        read_rc_end_kmer = guide.rc().substr(n.query_e-k, k);
-        if ( (ref_start_kmer == read_rc_start_kmer) && (ref_end_kmer == read_rc_end_kmer) ) { // && (ref_segm.substr(n.query_e - k + (ref_diff - read_diff), k) == read_rc.substr(n.query_e - k, k)) ){
-            n.is_rc = true;
-            fits = true;
-        } else{
-            //  FALSE REVERSE TAKE CARE OF FALSE HITS HERE - it can be false forwards or false rc because of symmetrical hash values
-            //    we need two extra checks for this - hopefully this will remove all the false hits we see (true hash collisions should be very few)
-
-            int q_start_tmp = guide_read_len - n.query_e;
-            int q_end_tmp = guide_read_len - n.query_s;
-            read_start_kmer = guide.seq.substr(q_start_tmp, k);
-            read_end_kmer = guide.seq.substr(q_end_tmp-k, k);
-//            std::cerr << " CHECKING2!! " <<   n.query_s << " " <<   n.query_e << " " << std::endl;
-//            std::cerr << read_start_kmer  << " " <<  ref_start_kmer << " " <<  read_end_kmer << " " << ref_end_kmer << std::endl;
-
-            if ((ref_start_kmer == read_start_kmer) && (ref_end_kmer == read_end_kmer)){
-                fits = true;
-                n.is_rc = false;
-                n.query_s = q_start_tmp;
-                n.query_e = q_end_tmp;
-//                std::cerr << " RESCUE DETECTED FALSE FW FROM SYMM!! " << std::endl;
-            }
-        }
-    }
-
+    revcomp_nam_if_needed(n, guide, references, k);
     if (n.is_rc){
         r_tmp = read.seq;
         a = n.ref_s - n.query_s - (mu+5*sigma);
