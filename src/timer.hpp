c652acf98161ff3376505b90677842de00e30014
Marcel Martin <marcel.martin@scilifelab.se>
2022-10-29 00:55:13 +0200

    Introduce a Timer class and use it

---
+++ src/timer.hpp
@@ -0,0 +1,20 @@
+#ifndef TIMER_HPP
+#define TIMER_HPP
+
+#include <chrono>
+
+// A timer that automatically starts on construction
+class Timer {
+public:
+    Timer() : start_time(std::chrono::high_resolution_clock::now()) {
+    }
+
+    // Return time elapsed since construction
+    std::chrono::duration<double> duration() const {
+        return std::chrono::high_resolution_clock::now() - start_time;
+    }
+private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> start_time;
+};
+
+#endif
