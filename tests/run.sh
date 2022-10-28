37f74a1ab842e5ff6e523dae9ff4dd4d65d192a3
Marcel Martin <marcel.martin@scilifelab.se>
2022-10-28 11:20:19 +0200

    Force correct read length on test data
    The read length is detected incorrectly at the moment. By using -r 300, we
    force it to the correct value.
    
    This changes mapping results. Some mapping qualities change from 0 to 60.
    Also, the second 'rescuable.42' read can no longer be rescued for some
    reason, so we tweak the sequence a bit (verified manually that it still
    triggers rescue).

--- tests/run.sh
+++ tests/run.sh
@@ -10,22 +10,22 @@ strobealign -h > /dev/null
 d=tests
 
 # Single-end SAM
-strobealign $d/phix.fasta $d/phix.1.fastq > phix.se.sam
+strobealign -r 300 $d/phix.fasta $d/phix.1.fastq > phix.se.sam
 diff -u tests/phix.se.sam phix.se.sam
 rm phix.se.sam
 
 # Paired-end SAM
-strobealign $d/phix.fasta $d/phix.1.fastq $d/phix.2.fastq > phix.pe.sam
+strobealign -r 300 $d/phix.fasta $d/phix.1.fastq $d/phix.2.fastq > phix.pe.sam
 diff -u tests/phix.pe.sam phix.pe.sam
 rm phix.pe.sam
 
 # Single-end PAF
-strobealign -x $d/phix.fasta $d/phix.1.fastq | tail > phix.se.paf
+strobealign -r 300 -x $d/phix.fasta $d/phix.1.fastq | tail > phix.se.paf
 diff -u tests/phix.se.paf phix.se.paf
 rm phix.se.paf
 
 # Paired-end PAF
-strobealign -x $d/phix.fasta $d/phix.1.fastq $d/phix.2.fastq | tail > phix.pe.paf
+strobealign -r 300 -x $d/phix.fasta $d/phix.1.fastq $d/phix.2.fastq | tail > phix.pe.paf
 diff -u tests/phix.pe.paf phix.pe.paf
 rm phix.pe.paf
 
