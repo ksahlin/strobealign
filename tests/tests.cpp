4ee22f21f79c600a4dc34a6d9ef39d828b36b444
Marcel Martin <marcel.martin@scilifelab.se>
2022-12-26 00:44:41 +0100

    Move tests for References to separate file

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -11,56 +11,6 @@
 #include "aln.hpp"
 
 
-TEST_CASE("References::add") {
-    References references;
-    references.add(std::string("thename"), std::string("ACGT"));
-
-    CHECK(references.names.size() == 1);
-    CHECK(references.names[0] == "thename");
-    CHECK(references.sequences[0] == "ACGT");
-    CHECK(references.lengths[0] == 4);
-}
-
-TEST_CASE("References::from_fasta") {
-    auto references = References::from_fasta("tests/phix.fasta");
-    CHECK(references.names.size() == 1);
-    CHECK(references.names[0] == "NC_001422.1");
-    CHECK(references.sequences.size() == 1);
-    CHECK(references.lengths.size() == 1);
-    CHECK(references.lengths[0] == 5386);
-    CHECK(references.total_length() == 5386);
-}
-
-TEST_CASE("References::from_fasta parse error") {
-    REQUIRE_THROWS_AS(References::from_fasta("tests/phix.1.fastq"), InvalidFasta);
-}
-
-TEST_CASE("Reference FASTA not found") {
-    REQUIRE_THROWS_AS(References::from_fasta("does-not-exist.fasta"), InvalidFasta);
-}
-
-TEST_CASE("Reference uppercase") {
-    {
-        std::ofstream ofs("tmpref.fasta");
-        ofs
-            << ">ref1\n"
-            << "acgt\n\n"
-            << ">ref2\n"
-            << "aacc\ngg\n\ntt\n"
-            << ">empty\n"
-            << ">empty_at_end_of_file";
-    }
-    auto refs = References::from_fasta("tmpref.fasta");
-    std::remove("tmpref.fasta");
-    CHECK(refs.sequences.size() == 2);
-    CHECK(refs.sequences[0].size() == 4);
-    CHECK(refs.sequences[0] == "ACGT");
-    CHECK(refs.sequences[1].size() == 8);
-    CHECK(refs.sequences[1] == "AACCGGTT");
-    CHECK(refs.names.size() == 2);
-    CHECK(refs.lengths.size() == 2);
-}
-
 TEST_CASE("estimate_read_length") {
     CHECK(estimate_read_length("tests/phix.1.fastq", "") == 289);
     CHECK(estimate_read_length("tests/phix.1.fastq", "tests/phix.2.fastq") == 289);
