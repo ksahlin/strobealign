b0ab1ae2d42788602435be455ad0d96fcceecd87
Marcel Martin <marcel.martin@scilifelab.se>
2023-01-24 13:43:57 +0100

    kind of doing something

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::min;
+use std::collections::VecDeque;
 use std::env;
 use std::fs::File;
 use std::io::{BufReader, BufRead, Error};
@@ -38,6 +40,159 @@ fn read_fasta<R: BufRead>(reader: &mut R) -> Result<Vec<FastaRecord>, Error> {
     Ok(records)
 }
 
+
+#[derive(Debug)]
+struct Syncmer {
+    hash: u64,
+    position: usize,
+}
+
+struct SyncmerIterator<'a> {
+    seq: &'a [u8],
+    k: u64,
+    s: u64,
+    t: u64,
+    kmask: u64,
+    smask: u64,
+    kshift: u64,
+    sshift: u64,
+    qs: VecDeque<u64>, // s-mer hashes
+    qs_min_val: u64,
+    qs_min_pos: usize,
+    l: u64,
+    xk: [u64; 2],
+    xs: [u64; 2],
+    i: usize,
+}
+
+impl<'a> SyncmerIterator<'a> {
+    fn new(seq: &'a [u8], k: u64, s: u64, t: u64) -> SyncmerIterator {
+        SyncmerIterator {
+            seq,
+            k,
+            s,
+            t,
+            kmask: (1 << 2*k) - 1,
+            smask: (1 << 2*s) - 1,
+            kshift: (k - 1) * 2,
+            sshift: (s - 1) * 2,
+            qs: VecDeque::new(),
+            qs_min_val: u64::MAX,
+            qs_min_pos: 0,
+            l: 0,
+            xk: [0, 0],
+            xs: [0, 0],
+            i: 0,
+        }
+    }
+}
+
+// a, A -> 0
+// c, C -> 1
+// g, G -> 2
+// t, T, u, U -> 3
+static NUCLEOTIDES: [u8; 256] = [
+        0, 1, 2, 3,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+        4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+        4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+        4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+        4, 0, 4, 1,  4, 4, 4, 2,  4, 4, 4, 4,  4, 4, 4, 4,
+        4, 4, 4, 4,  3, 3, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+        4, 0, 4, 1,  4, 4, 4, 2,  4, 4, 4, 4,  4, 4, 4, 4,
+        4, 4, 4, 4,  3, 3, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+        4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+        4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+        4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+        4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+        4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+        4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+        4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+        4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+];
+
+fn syncmer_kmer_hash(hash: u64) -> u64 {
+    hash
+}
+
+impl<'a> Iterator for SyncmerIterator<'a> {
+    type Item = Syncmer;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.i < self.seq.len() {
+            println!("self.i={}", self.i);
+            let ch = self.seq[self.i];
+            let c = NUCLEOTIDES[ch as usize];
+            if c < 4 { // not an "N" base
+                self.xk[0] = (self.xk[0] << 2 | (c as u64)) & self.kmask;                  // forward strand
+                self.xk[1] = self.xk[1] >> 2 | ((3 - c) as u64) << self.kshift;  // reverse strand
+                self.xs[0] = (self.xs[0] << 2 | (c as u64)) & self.smask;                  // forward strand
+                self.xs[1] = self.xs[1] >> 2 | ((3 - c) as u64) << self.sshift;  // reverse strand
+                self.l += 1;
+                if self.l < self.s {
+                    self.i += 1;
+                    continue;
+                }
+                // we find an s-mer
+                let ys = min(self.xs[0], self.xs[1]);
+                //          uint64_t hash_s = robin_hash(ys);
+                let hash_s = ys;
+                //          uint64_t hash_s = hash64(ys, mask);
+                //          uint64_t hash_s = XXH64(&ys, 8,0);
+                self.qs.push_back(hash_s);
+                println!("adding bitpacked syncmer {}", hash_s);
+                // not enough hashes in the queue, yet
+                if self.qs.len() < (self.k - self.s + 1) as usize {
+                    self.i += 1;
+                    continue;
+                }
+                if self.qs.len() == (self.k - self.s + 1) as usize { // We are at the last s-mer within the first k-mer, need to decide if we add it
+                    // TODO use argmin
+                    for j in 0..self.qs.len() {
+                        println!("self.qs[j]={} self.qs_min_val={} self.i={} j={} k={}", self.qs[j], self.qs_min_val, self.i, j, self.k);
+                        if self.qs[j] < self.qs_min_val {
+                            self.qs_min_val = self.qs[j];
+                            self.qs_min_pos = self.i + j + 1 - self.k as usize;
+                        }
+                    }
+                } else {
+                    // update queue and current minimum and position
+                    self.qs.pop_front();
+                    if self.qs_min_pos == self.i - self.k as usize { // we popped the previous minimizer, find new brute force
+                        self.qs_min_val = u64::MAX;
+                        self.qs_min_pos = self.i - self.s as usize + 1;
+                        for j in (0..self.qs.len()).rev() { //Iterate in reverse to choose the rightmost minimizer in a window
+                            if self.qs[j] < self.qs_min_val {
+                                self.qs_min_val = self.qs[j];
+                                self.qs_min_pos = self.i + j + 1 - self.k as usize;
+                            }
+                        }
+                    } else if hash_s < self.qs_min_val { // the new value added to queue is the new minimum
+                        self.qs_min_val = hash_s;
+                        self.qs_min_pos = self.i - self.s as usize + 1;
+                    }
+                }
+                if self.qs_min_pos == self.i + self.t as usize - self.k as usize { // occurs at t:th position in k-mer
+                    let yk = min(self.xk[0], self.xk[1]);
+                    let syncmer = Syncmer { hash: syncmer_kmer_hash(yk), position: self.i - self.k as usize + 1 };
+                    self.i += 1;
+                    return Some(syncmer);
+                }
+            } else {
+                println!("Found N at {}", self.i);
+                // if there is an "N", restart
+                self.qs_min_val = u64::MAX;
+                self.qs_min_pos = 0;
+                self.l = 0;
+                self.xs = [0, 0];
+                self.xk = [0, 0];
+                self.qs.clear();
+            }
+            self.i += 1;
+        }
+        None
+    }
+}
+
 fn main() -> Result<(), Error> {
     let args = &env::args().collect::<Vec<_>>();
     let path = Path::new(&args[1]);
@@ -45,7 +200,10 @@ fn main() -> Result<(), Error> {
     let mut reader = BufReader::new(f);
     let records = read_fasta(&mut reader).unwrap();
     for record in &records {
-        println!(">{}\n{}", record.name, String::from_utf8(record.sequence.to_vec()).unwrap());
+        println!(">{} {} bp", record.name, record.sequence.len());
+        for syncmer in SyncmerIterator::new(&record.sequence, 20, 16, 3) {
+            println!("syncmer {:?}", syncmer);
+        }
     }
     Ok(())
 }
