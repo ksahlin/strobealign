f575e85f0396c31d7152fd85084cd5282f896c5a
Luis Pedro Coelho <luis@luispedro.org>
2023-02-08 11:10:36 +0100

    ENH Parse FASTA files using a buffer
    Benchmarks showed that the previous commit made reading inputs slower.
    Reading in chunks and using memchr to jump ahead restores performance.

--- src/refs.cpp
+++ src/refs.cpp
@@ -1,8 +1,11 @@
 #include "refs.hpp"
 #include <vector>
 #include <fstream>
+#include <cstdio>
 #include <sstream>
 #include <algorithm>
+#include <assert.h>
+#include <cstring>
 #include <zlib.h>
 
 /* Convert string to uppercase in-place */
@@ -15,9 +18,6 @@ void to_uppercase(std::string& s) {
 
 namespace {
 
-unsigned char to_uppercase1(unsigned char c) {
-    return c & ~32;
-}
 class GZFile {
 public:
     GZFile(const std::string& filename, const char* mode) {
@@ -32,20 +32,76 @@ public:
     int getc() {
         return gzgetc(file_);
     }
+    int read(void* buf, unsigned int len) {
+        return gzread(file_, buf, len);
+    }
 
 private:
     gzFile file_;
 };
 
-enum struct State {
-    Start,
-    Header,
-    HeaderRest,
-    Sequence,
-    EOL
-};
-
+void fill_buffer(GZFile& file, char* buffer, size_t& buffer_size, size_t& buffer_pos, size_t max_buffer_size) {
+    buffer_pos = 0;
+    buffer_size = file.read(buffer, max_buffer_size);
 }
+
+std::string read_seq_name(GZFile& gf, char* buffer, size_t& buffer_size, const size_t max_buffer_size, size_t& buffer_pos) {
+    char* eol = static_cast<char*>(std::memchr(buffer + buffer_pos, '\n', buffer_size - buffer_pos));
+    if (eol) {
+        // Normal (simple) case. Given the large size of the buffer, we expect this to be the case most of the time.
+        char* space = static_cast<char*>(std::memchr(buffer + buffer_pos, ' ', eol - buffer + buffer_pos));
+        char* name_end = space ? space : eol;
+        std::string name{buffer + buffer_pos, name_end};
+        buffer_pos = eol - buffer + 1;
+        return name;
+    }
+
+    // We need to read more data, but first we save the data we already have
+    std::string name;
+    char* space = static_cast<char*>(std::memchr(buffer + buffer_pos, ' ', buffer_size - buffer_pos));
+    bool found_space = false;
+    if (space) {
+        name.assign(buffer + buffer_pos, space);
+        found_space = true;
+    } else {
+        name.assign(buffer + buffer_pos, buffer + buffer_size);
+    }
+    fill_buffer(gf, buffer, buffer_size, buffer_pos, max_buffer_size);
+    if (buffer_size == 0) return name;
+    eol = static_cast<char*>(std::memchr(buffer, '\n', buffer_size));
+    if (!eol) { // No EOL found. This file is malformed
+        throw InvalidFasta("Malformed FASTA file");
+    }
+    buffer_pos = eol - buffer + 1;
+    if (!found_space) {
+        space = static_cast<char*>(std::memchr(buffer, ' ', eol - buffer));
+        if (!space) space = eol;
+        name.append(buffer, space);
+    }
+    return name;
+}
+
+std::string read_sequence(GZFile& gf, char* buffer, size_t& buffer_size, const size_t max_buffer_size, size_t& buffer_pos) {
+    if (buffer_pos == buffer_size) {
+        fill_buffer(gf, buffer, buffer_size, buffer_pos, max_buffer_size);
+        if (buffer_size == 0) return "";
+    }
+    std::string seq;
+    while (buffer_size > 0 && buffer[buffer_pos] != '>') {
+        char* eol = static_cast<char*>(std::memchr(buffer + buffer_pos, '\n', buffer_size - buffer_pos));
+        if (eol) {
+            seq.append(buffer + buffer_pos, eol);
+            buffer_pos = eol - buffer + 1;
+        } else {
+            seq.append(buffer + buffer_pos, buffer + buffer_size);
+            fill_buffer(gf, buffer, buffer_size, buffer_pos, max_buffer_size);
+        }
+    }
+    to_uppercase(seq);
+    return seq;
+}
+}
+
 
 References References::from_fasta(const std::string& filename) {
     std::vector<std::string> sequences;
@@ -54,64 +110,31 @@ References References::from_fasta(const std::string& filename) {
     GZFile gf(filename, "r");
 
     std::string name, seq;
-    auto state = State::Start;
-    while (true) {
-        const int c = gf.getc();
-        if (c == EOF) {
-            if (seq != "") {
-                names.push_back(std::move(name));
-                sequences.push_back(std::move(seq));
-            }
+    char buffer[128 * 1024];
+    size_t buffer_size = 0;
+    size_t buffer_pos = 0;
+    buffer_size = gf.read(buffer, sizeof(buffer));
+    if (buffer_size <= 0) {
+        throw InvalidFasta("Could not read from file " + filename);
+    }
+    if (buffer[0] != '>') {
+        throw InvalidFasta("File " + filename + " does not start with >");
+    }
+
+    do {
+        // The read_sequence function should ensure that the buffer is not empty and at a '>' character
+        assert(buffer_pos < buffer_size && buffer[buffer_pos] == '>');
+        ++buffer_pos; // Skip the >
+        name = read_seq_name(gf, buffer, buffer_size, sizeof(buffer), buffer_pos);
+        if (name.empty()) {
             break;
         }
-        // If we see a newline, we move to EOL state and ignore the input
-        // character (except if it happens to be the very first character in
-        // the file).
-        if (c == '\n' && state != State::Start) {
-            state = State::EOL;
-            continue;
+        seq = read_sequence(gf, buffer, buffer_size, sizeof(buffer), buffer_pos);
+        if (!seq.empty()) {
+            sequences.push_back(std::move(seq));
+            names.push_back(std::move(name));
         }
-        switch (state) {
-            case State::Start:
-                if (c == '>') {
-                    state = State::Header;
-                } else {
-                    std::ostringstream oss;
-                    oss << "FASTA file ('" << filename << "') must begin with '>' character, not '"
-                        << static_cast<unsigned char>(c) << "'";
-                    throw InvalidFasta(oss.str());
-                }
-                break;
-            case State::Header:
-                if (c == ' ') {
-                    state = State::HeaderRest;
-                } else {
-                    name += static_cast<char>(c);
-                }
-                break;
-            case State::HeaderRest:
-                // Ignore everything until the end of the line
-                break;
-            case State::Sequence:
-                seq += to_uppercase1(static_cast<char>(c));
-                break;
-            case State::EOL:
-                if (c == '>') {
-                    state = State::Header;
-                    if (seq != "") {
-                        names.push_back(std::move(name));
-                        sequences.push_back(std::move(seq));
-                    }
-
-                    name.clear();
-                    seq.clear();
-                } else {
-                    seq += to_uppercase1(static_cast<char>(c));
-                    state = State::Sequence;
-                }
-                break;
-            }
-    }
+    } while (buffer_size > 0);
 
     return References(std::move(sequences), std::move(names));
 }
