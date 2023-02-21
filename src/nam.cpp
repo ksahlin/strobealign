4c6771626c6f6e3a62fc935cee0d179308b85995
Marcel Martin <marcel.martin@scilifelab.se>
2023-02-21 15:11:04 +0100

    Factor out merge_hits_into_nams()
    This eliminates a lot of duplication from find_nams() and find_nams_rescue()

--- src/nam.cpp
+++ src/nam.cpp
@@ -44,33 +44,22 @@ void add_to_hits_per_ref(
     }
 }
 
-/* Return the fraction of nonrepetitive hits (those not above the filter_cutoff threshold)
- *
- */
-float find_nams(
+void merge_hits_into_nams(
+    robin_hood::unordered_map<unsigned int, std::vector<hit>> hits_per_ref,
+    int k,
     std::vector<nam> &final_nams,
-    const QueryRandstrobeVector &query_randstrobes,
-    const StrobemerIndex& index
+    bool sort
 ) {
-    robin_hood::unordered_map<unsigned int, std::vector<hit>> hits_per_ref;
-    hits_per_ref.reserve(100);
-
-    int nr_good_hits = 0, total_hits = 0;
-    for (const auto &q : query_randstrobes) {
-        auto ref_hit = index.find(q.hash);
-        if (ref_hit != index.end()) {
-            total_hits++;
-            if (ref_hit->second.count() > index.filter_cutoff) {
-                continue;
-            }
-            nr_good_hits++;
-            add_to_hits_per_ref(hits_per_ref, q.start, q.end, q.is_reverse, index, ref_hit->second, 100'000);
-        }
-    }
-    float nonrepetitive_fraction = total_hits > 0 ? ((float) nr_good_hits) / ((float) total_hits) : 1.0;
     int nam_id_cnt = 0;
+    for (auto &[ref_id, hits] : hits_per_ref) {
+        if (sort) {
+            std::sort(hits.begin(), hits.end(), [](const hit& a, const hit& b) -> bool {
+                    // first sort on query starts, then on reference starts
+                    return (a.query_s < b.query_s) || ( (a.query_s == b.query_s) && (a.ref_s < b.ref_s) );
+                }
+            );
+        }
 
-    for (const auto &[ref_id, hits] : hits_per_ref) {
         std::vector<nam> open_nams;
         unsigned int prev_q_start = 0;
         for (auto &h : hits) {
@@ -125,7 +114,7 @@ float find_nams(
             }
 
             // Only filter if we have advanced at least k nucleotides
-            if (h.query_s > prev_q_start + index.k()) {
+            if (h.query_s > prev_q_start + k) {
 
                 // Output all NAMs from open_matches to final_nams that the current hit have passed
                 for (auto &n : open_nams) {
@@ -159,7 +148,33 @@ float find_nams(
             final_nams.push_back(n);
         }
     }
+}
 
+/* Return the fraction of nonrepetitive hits (those not above the filter_cutoff threshold)
+ */
+float find_nams(
+    std::vector<nam> &final_nams,
+    const QueryRandstrobeVector &query_randstrobes,
+    const StrobemerIndex& index
+) {
+    robin_hood::unordered_map<unsigned int, std::vector<hit>> hits_per_ref;
+    hits_per_ref.reserve(100);
+
+    int nr_good_hits = 0, total_hits = 0;
+    for (const auto &q : query_randstrobes) {
+        auto ref_hit = index.find(q.hash);
+        if (ref_hit != index.end()) {
+            total_hits++;
+            if (ref_hit->second.count() > index.filter_cutoff) {
+                continue;
+            }
+            nr_good_hits++;
+            add_to_hits_per_ref(hits_per_ref, q.start, q.end, q.is_reverse, index, ref_hit->second, 100'000);
+        }
+    }
+    float nonrepetitive_fraction = total_hits > 0 ? ((float) nr_good_hits) / ((float) total_hits) : 1.0;
+
+    merge_hits_into_nams(hits_per_ref, index.k(), final_nams, false);
     return nonrepetitive_fraction;
 }
 
@@ -215,103 +230,7 @@ void find_nams_rescue(
         }
     }
 
-    std::vector<nam> open_nams;
-    int nam_id_cnt = 0;
-
-    for (auto &it : hits_per_ref) {
-        auto ref_id = it.first;
-        std::vector<hit>& hits = it.second;
-        std::sort(hits.begin(), hits.end(), [](const hit& a, const hit& b) -> bool {
-                // first sort on query starts, then on reference starts
-                return (a.query_s < b.query_s) || ( (a.query_s == b.query_s) && (a.ref_s < b.ref_s) );
-            }
-        );
-        open_nams = std::vector<nam> (); // Initialize vector
-        unsigned int prev_q_start = 0;
-        for (auto &h : hits){
-            bool is_added = false;
-            for (auto & o : open_nams) {
-                // Extend NAM
-                if (o.is_rc == h.is_rc && o.query_prev_hit_startpos < h.query_s && h.query_s <= o.query_e && o.ref_prev_hit_startpos < h.ref_s && h.ref_s <= o.ref_e) {
-                    if (h.query_e > o.query_e && h.ref_e > o.ref_e) {
-                        o.query_e = h.query_e;
-                        o.ref_e = h.ref_e;
-//                        o.previous_query_start = h.query_s;
-//                        o.previous_ref_start = h.ref_s; // keeping track so that we don't . Can be caused by interleaved repeats.
-                        o.query_prev_hit_startpos = h.query_s; // log the last strobemer hit in case of outputting paf
-                        o.ref_prev_hit_startpos = h.ref_s; // log the last strobemer hit in case of outputting paf
-                        o.n_hits ++;
-//                        o.score += (float)1/ (float)h.count;
-                        is_added = true;
-                        break;
-                    }
-                    else if (h.query_e <= o.query_e && h.ref_e <= o.ref_e) {
-//                        o.previous_query_start = h.query_s;
-//                        o.previous_ref_start = h.ref_s; // keeping track so that we don't . Can be caused by interleaved repeats.
-                        o.query_prev_hit_startpos = h.query_s; // log the last strobemer hit in case of outputting paf
-                        o.ref_prev_hit_startpos = h.ref_s; // log the last strobemer hit in case of outputting paf
-                        o.n_hits ++;
-//                        o.score += (float)1/ (float)h.count;
-                        is_added = true;
-                        break;
-                    }
-                }
-            }
-            // Add the hit to open matches
-            if (!is_added){
-                nam n;
-                n.nam_id = nam_id_cnt;
-                nam_id_cnt ++;
-                n.query_s = h.query_s;
-                n.query_e = h.query_e;
-                n.ref_s = h.ref_s;
-                n.ref_e = h.ref_e;
-                n.ref_id = ref_id;
-//                n.previous_query_start = h.query_s;
-//                n.previous_ref_start = h.ref_s;
-                n.query_prev_hit_startpos = h.query_s;
-                n.ref_prev_hit_startpos = h.ref_s;
-                n.n_hits = 1;
-                n.is_rc = h.is_rc;
-//                n.score += (float)1/ (float)h.count;
-                open_nams.push_back(n);
-            }
-
-            // Only filter if we have advanced at least k nucleotides
-            if (h.query_s > prev_q_start + index.k()) {
-
-                // Output all NAMs from open_matches to final_nams that the current hit have passed
-                for (auto &n : open_nams) {
-                    if (n.query_e < h.query_s) {
-                        int n_max_span = std::max(n.query_span(), n.ref_span());
-                        int n_min_span = std::min(n.query_span(), n.ref_span());
-                        float n_score;
-                        n_score = ( 2*n_min_span -  n_max_span) > 0 ? (float) (n.n_hits * ( 2*n_min_span -  n_max_span) ) : 1;   // this is really just n_hits * ( min_span - (offset_in_span) ) );
-//                        n_score = n.n_hits * n.query_span();
-                        n.score = n_score;
-                        final_nams.push_back(n);
-                    }
-                }
-
-                // Remove all NAMs from open_matches that the current hit have passed
-                auto c = h.query_s;
-                auto predicate = [c](decltype(open_nams)::value_type const &nam) { return nam.query_e < c; };
-                open_nams.erase(std::remove_if(open_nams.begin(), open_nams.end(), predicate), open_nams.end());
-                prev_q_start = h.query_s;
-            }
-        }
-
-        // Add all current open_matches to final NAMs
-        for (auto &n : open_nams){
-            int n_max_span = std::max(n.query_span(), n.ref_span());
-            int n_min_span = std::min(n.query_span(), n.ref_span());
-            float n_score;
-            n_score = ( 2*n_min_span -  n_max_span) > 0 ? (float) (n.n_hits * ( 2*n_min_span -  n_max_span) ) : 1;   // this is really just n_hits * ( min_span - (offset_in_span) ) );
-//            n_score = n.n_hits * (n.query_e - n.query_s);
-            n.score = n_score;
-            final_nams.push_back(n);
-        }
-    }
+    merge_hits_into_nams(hits_per_ref, index.k(), final_nams, true);
 }
 
 std::ostream& operator<<(std::ostream& os, const nam& n) {
