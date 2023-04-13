6b081ad3a46f4961671b8f559c0c4ec14a106f30
Marcel Martin <marcel.martin@scilifelab.se>
2023-04-13 18:17:01 +0200

    Use a lambda to reduce duplicated code in add_to_hits_per_ref

--- src/nam.cpp
+++ src/nam.cpp
@@ -19,29 +19,23 @@ void add_to_hits_per_ref(
     RandstrobeMapEntry randstrobe_map_entry,
     int min_diff
 ) {
+    auto func = [&](const RefRandstrobe& rr) {
+        int ref_s = rr.position;
+        int ref_e = rr.position + rr.strobe2_offset() + index.k();
+        int diff = std::abs((query_e - query_s) - (ref_e - ref_s));
+        if (diff <= min_diff) {
+            hits_per_ref[rr.reference_index()].push_back(Hit{query_s, query_e, ref_s, ref_e, is_rc});
+            min_diff = diff;
+        }
+    };
     // Determine whether the hash table’s value directly represents a
     // ReferenceMer (this is the case if count==1) or an offset/count
     // pair that refers to entries in the flat_vector.
     if (randstrobe_map_entry.is_direct()) {
-        auto r = randstrobe_map_entry.as_ref_randstrobe();
-        int ref_s = r.position;
-        int ref_e = r.position + r.strobe2_offset() + index.k();
-        int diff = std::abs((query_e - query_s) - (ref_e - ref_s));
-        if (diff <= min_diff) {
-            hits_per_ref[r.reference_index()].push_back(Hit{query_s, query_e, ref_s, ref_e, is_rc});
-            min_diff = diff;
-        }
+        func(randstrobe_map_entry.as_ref_randstrobe());
     } else {
         for (size_t j = randstrobe_map_entry.offset(); j < randstrobe_map_entry.offset() + randstrobe_map_entry.count(); ++j) {
-            auto r = index.flat_vector[j];
-            int ref_s = r.position;
-            int ref_e = r.position + r.strobe2_offset() + index.k();
-
-            int diff = std::abs((query_e - query_s) - (ref_e - ref_s));
-            if (diff <= min_diff) {
-                hits_per_ref[r.reference_index()].push_back(Hit{query_s, query_e, ref_s, ref_e, is_rc});
-                min_diff = diff;
-            }
+            func(index.flat_vector[j]);
         }
     }
 }
