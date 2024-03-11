45911fa9ae83a234d7c6180a2c28d336b9fe7d3b
Itolstoganov <itolstoganov@gmail.com>
2024-03-11 14:36:42 +0100

    Fix dumpstrobes

--- src/dumpstrobes.cpp
+++ src/dumpstrobes.cpp
@@ -101,7 +101,7 @@ int run_dumpstrobes(int argc, char **argv) {
     }
 
     // Seeding
-    int r{150}, k{20}, s{16}, c{8}, l{1}, u{7}, digest{8};
+    int r{150}, k{20}, s{16}, c{8}, l{1}, u{7}, digest{24};
     int max_seed_len{};
 
     bool k_set{false}, s_set{false}, c_set{false}, max_seed_len_set{false}, l_set{false}, u_set{false};
