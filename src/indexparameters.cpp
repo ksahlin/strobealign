bb2c60350a700c89b6a3175c1919ce4c1513fc43
ksahlin <ksahlin@math.su.se>
2024-05-20 15:44:37 +0200

    Updated parameters according to optimization in issue #423

--- src/indexparameters.cpp
+++ src/indexparameters.cpp
@@ -36,14 +36,14 @@ struct Profile {
 static auto max{std::numeric_limits<int>::max()};
 
 static std::vector<Profile> profiles = {
-        Profile{ 50,  70, 17, -4, -2,  0},
-        Profile{ 75,  90, 20, -4, -3, -1},
-        Profile{100, 110, 18, -4,  1,  3},
+        Profile{ 50,  70, 16, -4, -2,  -1},
+        Profile{ 75,  90, 19, -4, -1, -1},
+        Profile{100, 110, 17, -4,  1,  4},
         Profile{125, 135, 20, -4,  1,  4},
-        Profile{150, 175, 22, -4,  3,  5},
-        Profile{250, 275, 24, -4,  4, 12},
-        Profile{300, 375, 24, -4,  5, 13},
-        Profile{400, max, 25, -6,  7, 13},
+        Profile{150, 175, 20, -4,  2,  6},
+        Profile{250, 275, 23, -4,  3, 13},
+        Profile{300, 375, 25, -4,  3, 14},
+        Profile{400, max, 27, -6,  5, 14},
     };
 
 /* Create an IndexParameters instance based on a given read length.
