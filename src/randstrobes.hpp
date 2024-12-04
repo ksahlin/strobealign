2fdeafbb5007a2ccc9352c2ac4ca3f07dc9c5b53
Marcel Martin <marcel.martin@scilifelab.se>
2024-12-04 13:55:54 +0100

    Use one less bit for randstrobe hash (TODO: adjust hardcoded 9 somewhere)

--- src/randstrobes.hpp
+++ src/randstrobes.hpp
@@ -17,7 +17,7 @@
 using syncmer_hash_t = uint64_t;
 using randstrobe_hash_t = uint64_t;
 
-static constexpr uint64_t RANDSTROBE_HASH_MASK = 0xFFFFFFFFFFFFFF00;
+static constexpr uint64_t RANDSTROBE_HASH_MASK = 0xFFFFFFFFFFFFFE00;
 
 struct RefRandstrobe {
 private:
@@ -30,7 +30,7 @@ public:
     RefRandstrobe() : m_hash_offset_flag(0), m_position(0), m_ref_index(0) { }
 
     RefRandstrobe(randstrobe_hash_t hash, uint32_t position, uint32_t ref_index, uint8_t offset)
-        : m_hash_offset_flag((hash & RANDSTROBE_HASH_MASK) | offset)
+        : m_hash_offset_flag((hash & RANDSTROBE_HASH_MASK) | (offset << 1))
         , m_position(position)
         , m_ref_index(ref_index)
     { }
@@ -50,7 +50,7 @@ public:
     }
 
     unsigned strobe2_offset() const {
-        return m_hash_offset_flag & 0xff;
+        return (m_hash_offset_flag >> 1) & 0xff;
     }
 
     randstrobe_hash_t hash() const {
