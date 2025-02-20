b80064734b4b8f75d09e892b509a1fba4566e094
Marcel Martin <marcel.martin@scilifelab.se>
2025-02-20 18:08:24 +0100

    Factor out cmd()

--- tests/test.rs
+++ tests/test.rs
@@ -2,21 +2,25 @@ use assert_cmd::Command;
 use std::fs::read;
 use predicates::prelude::*;
 
+fn cmd() -> Command {
+    Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap()
+}
+
 #[test]
 fn fail_without_arguments() {
-    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    let mut cmd = cmd();
     cmd.assert().failure();
 }
 
 #[test]
 fn fail_with_unknown_argument() {
-    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    let mut cmd = cmd();
     cmd.arg("-G").assert().failure();
 }
 
 #[test]
 fn success_when_printing_help() {
-    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    let mut cmd = cmd();
     cmd.arg("--help").assert().success();
     cmd.arg("-h").assert().success();
 }
@@ -26,7 +30,7 @@ fn single_end_sam() {
     // TODO --chunk-size 3
     // TODO -v
     let expected = String::from_utf8(read("tests/phix.se.sam").unwrap()).unwrap();
-    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    let mut cmd = cmd();
     let p = cmd.args(&["--no-PG", "--eqx", "--rg-id=1", "--rg=SM:sample", "--rg=LB:library", "tests/phix.fasta", "tests/phix.1.fastq"]);
     let a = p.assert();
     a.success().stdout(predicate::str::diff(expected));
@@ -36,7 +40,7 @@ fn single_end_sam() {
 fn paired_end_sam() {
     // TODO --chunk-size 3
     let expected = String::from_utf8(read("tests/phix.pe.sam").unwrap()).unwrap();
-    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    let mut cmd = cmd();
     let p = cmd.args(&["--no-PG", "--eqx", "--rg-id=1", "--rg=SM:sample", "--rg=LB:library", "tests/phix.fasta", "tests/phix.1.fastq", "tests/phix.2.fastq"]);
     let a = p.assert();
     a.success().stdout(predicate::str::diff(expected));
