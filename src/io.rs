394455b8749715668dcc45b88bcc378491772f7c
Marcel Martin <marcel.martin@scilifelab.se>
2025-02-21 10:41:52 +0100

    Test reading of multi-block gzips

--- src/io.rs
+++ src/io.rs
@@ -17,3 +17,33 @@ pub fn xopen<P: AsRef<Path>>(path: P) -> Result<Box<dyn Read + Send>, Error> {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use std::error::Error;
+    use std::io::{Read, Write};
+    use flate2::Compression;
+    use flate2::write::GzEncoder;
+    use temp_file::TempFileBuilder;
+    use super::xopen;
+    
+    #[test]
+    fn test_open_multi_block_gzip() -> Result<(), Box<dyn Error>>{
+        // Create a multi-block gzip
+        let mut encoder = GzEncoder::new(Vec::new(), Compression::fast());
+        encoder.write_all(b"abc")?;
+        let first = encoder.finish()?;
+        let mut encoder = GzEncoder::new(Vec::new(), Compression::fast());
+        encoder.write_all(b"def")?;
+        let second = encoder.finish()?;
+        let both = vec![first, second].concat();
+        let tmp = TempFileBuilder::new().suffix(".gz").build()?.with_contents(&both)?;
+        
+        let mut buf = vec![];
+        xopen(tmp.path()).unwrap().read_to_end(&mut buf)?;
+        
+        assert_eq!(buf, b"abcdef");
+        
+        Ok(())
+    }
+}
