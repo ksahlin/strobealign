676a84c53a07b424fe7a02b6be159daa8cec5c1b
Marcel Martin <marcel.martin@scilifelab.se>
2025-02-26 16:12:09 +0100

    Test reverse_complement

--- src/revcomp.rs
+++ src/revcomp.rs
@@ -24,3 +24,23 @@ const REVCOMP_TABLE: [u8; 256] = [
 pub fn reverse_complement(s: &[u8]) -> Vec<u8> {
     s.iter().rev().map(|ch| REVCOMP_TABLE[*ch as usize]).collect()
 }
+
+
+#[cfg(test)]
+mod test {
+    use crate::revcomp::reverse_complement;
+
+    #[test]
+    fn test_reverse_complement() {
+        assert_eq!(reverse_complement(b""), b"");
+        assert_eq!(reverse_complement(b""), b"");
+        assert_eq!(reverse_complement(b"A"), b"T");
+        assert_eq!(reverse_complement(b"C"), b"G");
+        assert_eq!(reverse_complement(b"G"), b"C");
+        assert_eq!(reverse_complement(b"T"), b"A");
+        assert_eq!(reverse_complement(b"TG"), b"CA");
+        assert_eq!(reverse_complement(b"AC"), b"GT");
+        assert_eq!(reverse_complement(b"ACG"), b"CGT");
+        assert_eq!(reverse_complement(b"AACGT"), b"ACGTT");
+    }
+}
