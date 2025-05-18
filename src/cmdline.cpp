27abd574cef55b06cdf2764595fffe3323873e96
ksahlin <ksahlin@math.su.se>
2025-05-18 13:33:20 +0200

    Typo fix. Default value is set to 0.7 internally

--- src/cmdline.cpp
+++ src/cmdline.cpp
@@ -90,7 +90,7 @@ CommandLineOptions parse_command_line_arguments(int argc, char** argv) {
     args::ValueFlag<int> h(parser, "INT", "Collinear chaining look back heuristic [50]", {'H'});
     args::ValueFlag<float> gd(parser, "FLOAT", "Collinear chaining diagonal gap cost [0.1]", {"gd"});
     args::ValueFlag<float> gl(parser, "FLOAT", "Collinear chaining gap length cost [0.05]", {"gl"});
-    args::ValueFlag<float> vp(parser, "FLOAT", "Collinear chaining best chain score threshold [0.9]", {"vp"});
+    args::ValueFlag<float> vp(parser, "FLOAT", "Collinear chaining best chain score threshold [0.7]", {"vp"});
 
     args::Group search(parser, "Search parameters:");
     args::Flag mcs(
