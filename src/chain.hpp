5aa263bc6911a839ca270abeaeed2a849f3b9edb
NicolasBuchin <nicolas.buchin@etudiant.univ-rennes.fr>
2025-06-23 14:04:25 +0200

    small fix for tabulation errors

--- src/chain.hpp
+++ src/chain.hpp
@@ -27,12 +27,11 @@ std::vector<Nam> get_chains(
     std::minstd_rand& random_engine
 );
 
-static inline float mg_log2(float x)
-{
-	union { float f; uint32_t i; } z = { x };
-	float log_2 = ((z.i >> 23) & 255) - 128;
-	z.i &= ~(255 << 23);
-	z.i += 127 << 23;
-	log_2 += (-0.34484843f * z.f + 2.02466578f) * z.f - 0.67487759f;
-	return log_2;
+static inline float mg_log2(float x) {
+    union { float f; uint32_t i; } z = { x };
+    float log_2 = ((z.i >> 23) & 255) - 128;
+    z.i &= ~(255 << 23);
+    z.i += 127 << 23;
+    log_2 += (-0.34484843f * z.f + 2.02466578f) * z.f - 0.67487759f;
+    return log_2;
 }
