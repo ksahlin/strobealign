bb823ab5ea7cea22a1ce4da92e357bab411fdc95
Marcel Martin <marcel.martin@scilifelab.se>
2025-09-26 12:02:41 +0200

    Count and report number of rescued hits
    Is-new-baseline: yes

--- src/main.cpp
+++ src/main.cpp
@@ -391,6 +391,8 @@ int run_strobealign(int argc, char **argv) {
             << std::setw(9) << static_cast<float>(statistics.hits.partial_filtered) / statistics.n_randstrobes * 100 << " %\n"
         << "    Partial randstrobe not found         " << std::setw(12) << statistics.hits.partial_not_found
             << std::setw(9) << static_cast<float>(statistics.hits.partial_not_found) / statistics.n_randstrobes * 100 << " %\n"
+        << '\n'
+        << "    Filtered but rescued randstrobes     " << std::setw(12) << statistics.hits.rescued
         << "\nFound chains:                            " << std::setw(12) << statistics.n_nams
         << "              Per read: " << std::setw(7) << static_cast<float>(statistics.n_nams) / statistics.n_reads << std::endl
         << "\n## Rescue (-R)\n\n"
